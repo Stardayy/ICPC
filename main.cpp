7861e4a7c1b76c122c6d2a5dd43bc1553824785d
Star <triben21806@gmail.com>
2025-05-20 12:38:58 +0700

    Base file main

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define db double
+#define f0(i,n) for(int i=0;i<n;++i)
+#define f1(i,n) for(int i=1;i<=n;++i)
+#define fi first
+#define se second
+#define pb push_back
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpi;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
+const ll mod = (ll)1e9 + 7;
+const int MAXN = (int)1e5 + 5;
+
+void Solve()
+{
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+    {
+        Solve();
+    }
+    return 0;
+}
